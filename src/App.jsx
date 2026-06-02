a27f069c1a8555a6a2b11605dcd0d2a319b82d11
dipa-khadka <dipshanakhadka77@gmail.com>
2026-06-02 11:27:38 +0900

    my first react project

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import React from 'react'
+import {BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom'
+import './App.css'
+import Contact from './component/contact'
+import "./NavLinkTest.css";
+import Products from './component/Products'
+import ProductDetail from './component/ProductDetail'
+
+const products = [
+  {id : 1, name : "Laptop", price : 1200, image : "/image/laptop.png"},
+  {id : 2, name : "Keyboard", price : 800, image : "/image/keyboard.png"},
+  {id : 3, name : "Mouse", price : 500, image : "/image/mouse.png"}
+]
+
+const Home = () => <h2>Welcome to my Home </h2>
+function About(){
+  return(
+    <h2>This is About Page</h2>
+  )
+}
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <nav>
+      <NavLink to="/">Home </NavLink>
+      <NavLink to="/about">About </NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/productlist">Product List</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/productlist" element={<Products list = {products} />}></Route>
+       <Route path="/productlist/:id" element={<ProductDetail list = {products} />}></Route>
+    </Routes>
+    </BrowserRouter>
+     
+           
+    </>
+  )
+}
+
+export default App
